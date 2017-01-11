7cea6b4c663ca8c012a12d093ddc32b4e70945d7
Pie27 <ptm992@uowmail.edu.au>
2017-01-11 14:29:01 +0630

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup 
+  def setup
   	@user = User.new(name: "Example User", email:"user@example.com",
       password: "foobar", password_confirmation: "foobar")
   end
@@ -38,9 +38,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-    duplicate_user = @user.dup 
+    duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
-    @user.save 
+    @user.save
     assert_not duplicate_user.valid?
   end
 
@@ -48,12 +48,15 @@ class UserTest < ActiveSupport::TestCase
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email 
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password should be present (nonblank)" do 
-    @user.password = @user.password_confirmation = " " * 6 
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end 
 end
